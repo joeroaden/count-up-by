2225b31deb3f8e057f4c3fa06a6ebfbaa6709679
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-08 15:45:00 -0700

    include utility logic for solving for NAN and blank input

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
+//Utility Logic//
+function noInput(number1, number2) {
+  return ((number1.length===0) || (number2.length===0));
+}
+function notNumber(number1, number2) {
+  if (isNaN(number1, number2)) {
+    return 'Not a Number!';
+  }
+}
+
+
+
+
+
 //Business Logic//
 function forLoop(number1, number2) {
 let array = [];
 let result = [];
-for (let index = 0; index <= parseInt(number1); index += parseInt(number2)) {
+
+  if (noInput(number1, number2)) {
+    return 0;
+  }
+  if (notNumber(number1, number2)) {
+    return "Not a Number!";
+  }
+
+  for (let index = 0; index <= parseInt(number1); index += parseInt(number2)) {
   result.push(index);
 }
 return result
